f7eced802810c5e6ca7976977f5373689885f927
Daniel Quisbert <daniel.quisbert@et.bo>
2023-11-29 15:14:33 -0400

    add changes

---
+++ git-command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Limpia el proyecto Flutter
+git add .
+
+# Obtiene las dependencias
+git commit -m "add changes"
+
+# Notifica que se han completado los comandos
+git push origin master
+
